c8a7886992470ed1e007ed1c2da768d5a37d33e2
brendanlegas <brendanlegas@gmail.com>
2022-02-09 21:59:24 -0600

    version 1

---
+++ Assets/Scripts/Rotator2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator2 : MonoBehaviour
+{
+    [Header("Rotation Controls")]
+
+    [Tooltip("Axis around which the gameObject is rotating")]
+    [SerializeField]
+    private Vector3 rotationAxis = Vector3.up;
+
+
+    [Tooltip("The speed with which the gameObject is rotating. Measured in degrees/frame")]
+    [SerializeField]
+    private float rotationSpeed = 1f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationAxis, rotationSpeed);
+        
+    }
+}
